6cbbae27bf5f18f57f17d74643c01c5875e5992a
FrankLazaroTIC <2023_frank.lazaro@iticbcn.cat>
2023-10-15 17:57:59 +0200

    Primera versio 10000% convert.js

--- convert.js
+++ convert.js
@@ -5,7 +5,7 @@ function convertirCF() {
         let cent = (temp-32)/(9.0/5.0);
         console.log(`${temp}F -> ${cent}C`);
     }else if (CF.toUpperCase() == "F"){
-        let fh = (9/5) * temp + 32;
+        let fh = (9.0/5) * temp + 32;
         console.log(`${temp}C -> ${fh}F`);
     }else {
         console.log("Només es pot C ó F");
